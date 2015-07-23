d913f954be4b312ed3b873d9b5e12ce99f278743
wxlTest <lovlovelove888@163.com>
2015-07-23 11:13:14 +0800

    clear

--- e.cs
+++ e.cs
@@ -16,7 +16,7 @@ using System;
 namespace Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Sinks.Database
 {
     /// <summary>
-    /// Represents an event entry to be persisted in a database. master branch log
+    /// Represents an event entry to be persisted in a database.
     /// </summary>
     public class EventRecord
     {
